87eef919c66780f0f5f35627b4f64319be57f28f
Alvallesteros <angelo.alvarez@student.ateneo.edu>
2024-02-06 12:55:16 +0800

    Reogranized CSCI 51.01

---
+++ CSCI 51.02/practice.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int x;
+    cout << x;
+    cout << "Enter an integer: ";
+    cin >> x; // cin >> name; CSCI 51.02 ...
+    cout << "Received input: " << x;
+    cout << endl << "Added 2, now: ";
+    cout << (x+2) << endl;
+    return 0;
+}
